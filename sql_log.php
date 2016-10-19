bd6cdcba54117f6bbed4f38d8047d318ab9fbb79
Jari Luukkonen <Dragonius@users.noreply.github.com>
2016-10-19 13:47:39 +0300

    Update sql_log.php

--- sql_log.php
+++ sql_log.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "185.52.3.33";
-$username = "tzcrew";
-$password = "F7Cj9dbHhSkst2hj";
-$dbname = "tzcrew";
+$servername = "servername";
+$username = "servername";
+$password = "servername";
+$dbname = "servername";
 $con = mysqli_connect($servername,$username,$password,$dbname );
 ?>
