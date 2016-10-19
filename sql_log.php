91eea46c9218d6cbef1cb9d835c5c5fae1005f95
Jari Luukkonen <Dragonius@users.noreply.github.com>
2016-10-19 13:47:55 +0300

    Delete sql_log.php

--- sql_log.php
+++
@@ -1,7 +0,0 @@
-<?php
-$servername = "servername";
-$username = "servername";
-$password = "servername";
-$dbname = "servername";
-$con = mysqli_connect($servername,$username,$password,$dbname );
-?>
