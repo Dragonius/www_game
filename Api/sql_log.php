4760b2e58e02a368f44f0f041fe1140f3ff64b90
Jari Luukkonen <Dragonius@users.noreply.github.com>
2017-05-27 16:24:36 +0300

    Update sql_log.php

--- Api/sql_log.php
+++ Api/sql_log.php
@@ -3,5 +3,5 @@ $servername = "127.0.0.1";
 $username = "127.0.0.1";
 $password = "127.0.0.1";
 $dbname = "127.0.0.1";
-$con=mysqli_connect($servername,$username,$password,$dbname);
+$con=mysqli_connect($servername, $username, $password, $dbname);
 ?>
