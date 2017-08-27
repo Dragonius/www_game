6d01d1e83180443c38e9e8af42a3baa0bf9edb98
Jari Luukkonen <Dragonius@users.noreply.github.com>
2017-08-27 11:34:11 +0300

    CamelCase fixes

--- Api/fleet_ships.php
+++ Api/fleet_ships.php
@@ -1,24 +1,24 @@
 <?php
 
-class Fleet_ships {
-	private $fleet_ships;
+class Fleet_Ships {
+	private $fleet_Ships;
 
-	public function __construct($fleet_ships) {
-		$this->fleet_ships = $fleet_ships;
+	public function __construct($fleet_Ships) {
+		$this->fleet_ships = $fleet_Ships;
 	}
 
  	public function Ships_in_fleet() {
 		include("./Api/sql_log.php");
 		$user_check = $_SESSION['login_user'];
 	
-		$fleet_ships = "select Fleet.fleet, Fleet.ship from Account, Fleet, Base, Ship where Base.fleet=Fleet.fleet and
+		$fleet_Ships = "select Fleet.fleet, Fleet.ship from Account, Fleet, Base, Ship where Base.fleet=Fleet.fleet and
 		Fleet.ship=Ship.name_id and Account.base=Base.base and Account.name='$user_check'";
-		$fleet_ships2=$con->query($fleet_ships);
-		$fleet_fleet = mysqli_fetch_assoc($fleet_ships2);
+		$fleet_Ships2=$con->query($fleet_Ships);
+		$fleet_fleet = mysqli_fetch_assoc($fleet_Ships2);
 		echo "Your Current fleet: <B>" . $fleet_fleet ["fleet"] .
 		"</B><br> ships: <B>" . $fleet_fleet["ship"] . "</B>";
-		while($fleet_ships3 = $fleet_ships2->fetch_assoc()) {
-			echo " <B> " . $fleet_ships3["ship"] . " </B> " ;
+		while($fleet_Ships3 = $fleet_Ships2->fetch_assoc()) {
+			echo " <B> " . $fleet_Ships3["ship"] . " </B> " ;
 	}
         
     }	
