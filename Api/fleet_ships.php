5f091390a06d8a9573f2c5f130c01a9e100aa04a
Jari Luukkonen <Dragonius@users.noreply.github.com>
2017-08-28 09:25:21 +0300

    Camelcase fixes part2

--- Api/fleet_ships.php
+++ Api/fleet_ships.php
@@ -1,24 +1,24 @@
 <?php
 
-class Fleet_Ships {
-	private $fleet_Ships;
+class FleetShips {
+	private $fleetShips;
 
-	public function __construct($fleet_Ships) {
-		$this->fleet_ships = $fleet_Ships;
+	public function __construct($fleetShips) {
+		$this->fleetships = $fleetShips;
 	}
 
- 	public function Ships_in_fleet() {
+ 	public function Shipsinfleet() {
 		include("./Api/sql_log.php");
 		$user_check = $_SESSION['login_user'];
 	
-		$fleet_Ships = "select Fleet.fleet, Fleet.ship from Account, Fleet, Base, Ship where Base.fleet=Fleet.fleet and
+		$fleetShips = "select Fleet.fleet, Fleet.ship from Account, Fleet, Base, Ship where Base.fleet=Fleet.fleet and
 		Fleet.ship=Ship.name_id and Account.base=Base.base and Account.name='$user_check'";
-		$fleet_Ships2=$con->query($fleet_Ships);
-		$fleet_fleet = mysqli_fetch_assoc($fleet_Ships2);
-		echo "Your Current fleet: <B>" . $fleet_fleet ["fleet"] .
-		"</B><br> ships: <B>" . $fleet_fleet["ship"] . "</B>";
-		while($fleet_Ships3 = $fleet_Ships2->fetch_assoc()) {
-			echo " <B> " . $fleet_Ships3["ship"] . " </B> " ;
+		$fleetShips2=$con->query($fleetShips);
+		$fleetfleet = mysqli_fetch_assoc($fleetShips2);
+		echo "Your Current fleet: <B>" . $fleetfleet ["fleet"] .
+		"</B><br> ships: <B>" . $fleetfleet["ship"] . "</B>";
+		while($fleetShips3 = $fleetShips2->fetch_assoc()) {
+			echo " <B> " . $fleetShips3["ship"] . " </B> " ;
 	}
         
     }	
