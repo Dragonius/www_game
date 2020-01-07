3b696a3b0848994713142f3e3769fdc4573815aa
Jari Luukkonen <Dragonius@users.noreply.github.com>
2020-01-07 08:38:24 +0200

    Update buy_update.php

--- buy_update.php
+++ buy_update.php
@@ -1,48 +1,48 @@
 <?php
 //debug data of session data
-	include('session.php');
-	echo "fetch2    " .	$fetch2['session'] . "<br>";
-	echo "session2  " . $session2 . "<br>" ;
-	echo "session3  " . $session3 . "<br>" ;
-	$diff=$session3-$fetch2['session'];
-	echo "Diffrence " . $diff . "<br>" ;
-	echo "query2    " . $query2  . "<br>";
+    include('session.php');
+    echo "fetch2    " .    $fetch2['session'] . "<br>";
+    echo "session2  " . $session2 . "<br>" ;
+    echo "session3  " . $session3 . "<br>" ;
+    $diff=$session3-$fetch2['session'];
+    echo "Diffrence " . $diff . "<br>" ;
+    echo "query2    " . $query2  . "<br>";
 
 ?>
 <?php
-#Jos pyydetaan Post muodossa tee nämä
+#Jos pyydetaan Post muodossa tee nÃ¤mÃ¤
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	#Dumpaa kaikki buy_ship sisältö
-	#var_dump($_POST['buy_ship']);
-	#var_dump($_POST['ship_prize']);
-	#Linkitä Post Buy_ship muuttujaksi
-	$buy_ship = ($_POST['buy_ship']);
-	#Need to converter to Int
-	#$ship_prize = ($_POST['ship_prize']);
-	#If ship_prize is number, covenrt to int
-	$ship_prize = (is_numeric($_POST['ship_prize']) ? (int)$_POST['ship_prize'] : 0);
-	#Check what fleet user is in  login_user
-	//$query_buy= "INSERT INTO tzcrew.Fleet (`id`, `fleet`, `ship`, `damage`) VALUES ('', $fetch_buy, $buy_avaible3[0], '0') where Account.base=Base.base and Base.fleet=Fleet.fleet and Account.name='$user_check'";
-	#no fleet check just query against username
-	$query_buy= "INSERT INTO Fleet(fleet, ship, damage) VALUES ('fleet1', '$buy_ship', '0') ";
-	$query_cash="Select Base.money from Base, Account where Account.base=Base.base and Account.name='$user_check'";
-	$result_current_cash=$con->query($query_cash);
-	#need to conversion to int 
-	$count = $result_current_cash->fetch_assoc();               
-	$result_current_cash = $count['money'] ;
-	#end of conversion
-	#remove money
-	$result_prize=$result_current_cash-$ship_prize;
-	#Make right Query againstst "username"
-	$query_takecash="UPDATE Base INNER JOIN Account ON Base.base=Account.base SET Base.money='$result_prize' where Account.name='$user_check'";
-	#make query 
-	$result_cash=$con->query($query_takecash);
-	#make query 
-	$result_buy=$con->query($query_buy);
-	#Move user to back buy page
-	header("location: buy.php");
-	}
+    #Dumpaa kaikki buy_ship sisÃ¤ltÃ¶
+    #var_dump($_POST['buy_ship']);
+    #var_dump($_POST['ship_prize']);
+    #LinkitÃ¤ Post Buy_ship muuttujaksi
+    $buy_ship = ($_POST['buy_ship']);
+    #Need to converter to Int
+    #$ship_prize = ($_POST['ship_prize']);
+    #If ship_prize is number, covenrt to int
+    $ship_prize = (is_numeric($_POST['ship_prize']) ? (int)$_POST['ship_prize'] : 0);
+    #Check what fleet user is in  login_user
+    //$query_buy= "INSERT INTO tzcrew.Fleet (`id`, `fleet`, `ship`, `damage`) VALUES ('', $fetch_buy, $buy_avaible3[0], '0') where Account.base=Base.base and Base.fleet=Fleet.fleet and Account.name='$user_check'";
+    #no fleet check just query against username
+    $query_buy= "INSERT INTO Fleet(fleet, ship, damage) VALUES ('fleet1', '$buy_ship', '0') ";
+    $query_cash="Select Base.money from Base, Account where Account.base=Base.base and Account.name='$user_check'";
+    $result_current_cash=$con->query($query_cash);
+    #need to conversion to int 
+    $count = $result_current_cash->fetch_assoc();               
+    $result_current_cash = $count['money'] ;
+    #end of conversion
+    #remove money
+    $result_prize=$result_current_cash-$ship_prize;
+    #Make right Query againstst "username"
+    $query_takecash="UPDATE Base INNER JOIN Account ON Base.base=Account.base SET Base.money='$result_prize' where Account.name='$user_check'";
+    #make query 
+    $result_cash=$con->query($query_takecash);
+    #make query 
+    $result_buy=$con->query($query_buy);
+    #Move user to back buy page
+    header("location: buy.php");
+    }
 else {
-	header("location: welcome.php");
-	}
-	?>
+    header("location: welcome.php");
+    }
+    ?>
