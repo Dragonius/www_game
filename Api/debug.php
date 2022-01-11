2ee82d3657924612c3c2c75276a7572dae0f3506
Dragonius <Blackerhawker@hotmail.com>
2022-01-11 13:37:58 +0200

    founded new files. add them

---
+++ Api/debug.php
@@ -0,0 +1,23 @@
+<?php
+
+class Debug_data {
+	private $Debug_data;
+
+	public function __construct($Debug_data) {
+		$this->Debug_data = $Debug_data;
+	}
+
+ 	public function Debugdata() {
+		include("./Api/sql_log.php");
+		$user_check = $_SESSION['login_user'];
+	
+		echo "fetch2    " .	$fetch2['session'] . "<br>";
+		echo "session2  " . $session2 . "<br>" ;
+		echo "session3  " . $session3 . "<br>" ;
+		$diff=$session3-$fetch2['session'];
+		echo "Diffrence " . $diff . "<br>" ;
+		echo "query2    " . $query2  . "<br>";
+	
+	}
+}
+?>
