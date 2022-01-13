e30b2e4563a589dadcf407ce0d8441acfd29e697
Dragonius <Blackerhawker@hotmail.com>
2022-01-13 14:28:12 +0200

    fix typo

--- Api/add_ship_fleet.php
+++ Api/add_ship_fleet.php
@@ -11,16 +11,16 @@ class Addshipfleet {
 		include("./Api/sql_log.php");
 		$user_check = $_SESSION['login_user'];
 	#Select all fleet ships 
-		$addshipfleet = "SELECT ticker.id,
-        ticker.tick, ticker.player ,
-        ticker.building AS building,
-        ticker.complete,
-        ticker.last_tick,
-        ticker.last_tick - ticker.complete AS diffrence
+		$addshipfleet = "SELECT Ticker.id,
+        Ticker.tick, Ticker.player ,
+        Ticker.building AS building,
+        Ticker.complete,
+        Ticker.last_tick,
+        Ticker.last_tick - Ticker.complete AS diffrence
         FROM   ticker, base, account
         WHERE  account.name = '$user_check'
-        AND ticker.player = account. name
-        AND ticker.complete < ticker.last_tick";
+        AND Ticker.player = account. name
+        AND Ticker.complete < Ticker.last_tick";
 	#make query
 		$addshipfleet2=$con->query($addshipfleet);
 	#a lonk there are data -> display it on website
